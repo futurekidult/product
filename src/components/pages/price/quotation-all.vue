e3688d65e83d194c7880fe389017524e1cef0264
JingjingHelen <312162874@qq.com>
2022-03-15 09:56:28 +0800

    feat(price): add components design

---
+++ src/components/pages/price/quotation-all.vue
@@ -0,0 +1,13 @@
+<template>
+  <quotation-description />
+</template>
+
+<script>
+import QuotationDescription from './quotation-description.vue';
+
+export default {
+  components: {
+    QuotationDescription
+  }
+};
+</script>
---
+++ src/components/pages/price/quotation-list.vue
@@ -0,0 +1,14 @@
+<template>
+  <base-breadcrumb />
+  <quotation-all />
+</template>
+
+<script>
+import QuotationAll from './quotation-all.vue';
+
+export default {
+  components: {
+    QuotationAll
+  }
+};
+</script>
