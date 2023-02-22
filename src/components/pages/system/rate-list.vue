f6fd7fda0d42659051420927b3cb824367085e92
JingjingHelen <312162874@qq.com>
2023-02-22 11:43:51 +0800

    refactor(system): optimize rate part

--- src/components/pages/system/rate-list.vue
+++ src/components/pages/system/rate-list.vue
@@ -12,67 +12,30 @@
           新增
         </el-button>
       </div>
-      <div v-loading="$store.state.system.rateListLoading">
-        <el-table
-          stripe
-          border
-          empty-text="无数据"
-          :header-cell-style="{ background: '#eef1f6', color: '#606266' }"
-          :data="rateList"
-        >
-          <el-table-column
-            label="ID"
-            prop="id"
-            width="60px"
-          />
-          <el-table-column
-            label="美元"
-            prop="usd_to_cny"
-          />
-          <el-table-column
-            label="欧元"
-            prop="eur_to_cny"
-          />
-          <el-table-column
-            label="英镑"
-            prop="gbp_to_cny"
-          />
-          <el-table-column
-            label="日元"
-            prop="jpy_to_cny"
-          />
-          <el-table-column
-            label="加拿大元"
-            prop="cad_to_cny"
-          />
-          <el-table-column
-            label="创建时间"
-            prop="create_time"
-          />
-          <el-table-column label="操作">
-            <template #default="scope">
-              <text-btn @handle-click="showDeleteDialog(scope.row.id)">
-                删除
-              </text-btn>
-            </template>
-          </el-table-column>
-        </el-table>
-
-        <base-pagination
-          :length="$store.state.system.rateListLength"
-          :current-page="currentPage"
-          :page-num="pageSize"
-          @change-size="changePageSize"
-          @change-page="changeCurrentPage"
-        />
-      </div>
+      <base-table
+        v-loading="$store.state.system.rateListLoading"
+        :table-data="rateList"
+        :operation-width="100"
+        :pagination="pagination"
+        :length="$store.state.system.rateListLength"
+        :table-column="tableColumn"
+        :operation-visible="false"
+        @change-pagination="changePagination"
+      >
+        <template #default="slotProps">
+          <text-btn @handle-click="showDeleteDialog(slotProps.row.id)">
+            删除
+          </text-btn>
+        </template>
+      </base-table>
     </div>
-
+    <!-- 新增汇率弹窗 -->
     <el-dialog
       v-model="addVisible"
       title="新增"
       width="30%"
       :close-on-click-modal="false"
+      @close="closeForm"
     >
       <div class="content">
         注: 填写的数值为对应币种的100倍兑人民币的汇率值, 例如:
@@ -151,36 +114,11 @@
         </div>
       </el-form>
     </el-dialog>
-
-    <el-dialog
-      v-model="deleteDialogVisible"
-      title="提示"
-      width="25%"
-      :close-on-click-modal="false"
-    >
-      <div class="result-content">
-        是否删除该汇率
-      </div>
-      <div style="text-align: center">
-        <el-button
-          class="close-btn"
-          @click="closeDeleteDialog"
-        >
-          取消
-        </el-button>
-        <el-button
-          type="primary"
-          @click="deleteRate"
-        >
-          确定
-        </el-button>
-      </div>
-    </el-dialog>
   </div>
 </template>
 
 <script>
-import { formatterTime } from '../../../utils';
+import { formatterTime, resetFormFields } from '../../../utils';
 export default {
   data() {
     return {
@@ -219,9 +157,35 @@ export default {
           }
         ]
       },
-      deleteDialogVisible: false,
-      currentPage: 1,
-      pageSize: 10
+      pagination: this.$global.pagination,
+      tableColumn: [
+        { prop: 'id', label: 'ID', width: 80, fixed: 'left' },
+        {
+          prop: 'usd_to_cny',
+          label: '美元'
+        },
+        {
+          prop: 'eur_to_cny',
+          label: '欧元'
+        },
+        {
+          prop: 'gbp_to_cny',
+          label: '英镑'
+        },
+        {
+          prop: 'jpy_to_cny',
+          label: '日元'
+        },
+        {
+          prop: 'cad_to_cny',
+          label: '加拿大元'
+        },
+        {
+          prop: 'create_time',
+          label: '创建时间',
+          width: 200
+        }
+      ]
     };
   },
   mounted() {
@@ -230,10 +194,7 @@ export default {
   methods: {
     async getRateList() {
       this.$store.commit('system/setRateListLoading', true);
-      let params = {
-        current_page: this.currentPage,
-        page_size: this.pageSize
-      };
+      let params = this.pagination;
       try {
         await this.$store.dispatch('system/getRateList', { params });
         this.rateList = this.$store.state.system.rateList;
@@ -249,6 +210,8 @@ export default {
       try {
         await this.$store.dispatch('system/createRate', body);
         this.addVisible = false;
+        this.pagination.current_page = 1;
+        resetFormFields(this.$refs.rateForm);
         this.getRateList();
       } catch (err) {
         return;
@@ -261,37 +224,15 @@ export default {
         }
       });
     },
-    async deleteRate() {
-      try {
-        await this.$store.dispatch('system/deleteRate', {
-          id: this.deleteId
-        });
-        this.deleteDialogVisible = false;
-        this.getRateList();
-      } catch (err) {
-        return;
-      }
-    },
     showAddForm() {
       this.addVisible = true;
     },
     closeForm() {
       this.addVisible = false;
+      resetFormFields(this.$refs.rateForm);
     },
-    showDeleteDialog(id) {
-      this.deleteDialogVisible = true;
-      this.deleteId = id;
-    },
-    closeDeleteDialog() {
-      this.deleteDialogVisible = false;
-    },
-    changeCurrentPage(val) {
-      this.currentPage = val;
-      this.getRateList();
-    },
-    changePageSize(val) {
-      this.pageSize = val;
-      this.currentPage = 1;
+    changePagination(pagination) {
+      this.pagination = pagination;
       this.getRateList();
     }
   }
