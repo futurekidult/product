7d6bbcae148625b6565e0339ad080152fe37b37c
JingjingHelen <312162874@qq.com>
2023-03-07 16:27:56 +0800

    fix(solution): solve state value when approval refuse

--- src/components/pages/product/process/survey/product-plan.vue
+++ src/components/pages/product/process/survey/product-plan.vue
@@ -641,19 +641,24 @@ export default {
         product_id: +this.$route.params.productId
       };
       let funcName = '';
-      body.state = type;
       switch (this.progress.state) {
         case 5:
+          body.state = type;
           body.id = this.$store.state.product.survey.plan.plan.survey_id;
           body['survey_schedule_id'] = this.progress.id;
           funcName = 'product/survey/plan/submitPlan';
           break;
         case 10:
+          body.state = type;
           body['survey_schedule_id'] = this.progress.id;
           funcName = 'product/survey/plan/submitPlan';
           break;
         case 30:
+          body.state = val === 5 ? 35 : 20;
+          body.id = this.productForm.id;
+          funcName = 'product/survey/plan/updatePlan';
         case 35:
+          body.state = val === 5 ? 35 : 20;
           body.id = this.productForm.id;
           funcName = 'product/survey/plan/updatePlan';
           break;
